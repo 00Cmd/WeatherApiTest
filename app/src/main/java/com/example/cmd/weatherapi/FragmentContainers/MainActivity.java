e4a3845bd7322798f2fc99ea5ad4f43931af6bc1
Kristjan Tampere <dacmd@mail.ru>
2018-03-06 21:20:14 +0200

    commitment

--- app/src/main/java/com/example/cmd/weatherapi/FragmentContainers/MainActivity.java
+++ app/src/main/java/com/example/cmd/weatherapi/FragmentContainers/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.cmd.weatherapi;
+package com.example.cmd.weatherapi.FragmentContainers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.cmd.weatherapi.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/cmd/weatherapi/REST/WeatherService.java
@@ -0,0 +1,8 @@
+package com.example.cmd.weatherapi.REST;
+
+/**
+ * Created by cmd on 6.03.18.
+ */
+
+public class WeatherService {
+}
